a6b4ce0c288ff95ecd62eb8f7914e124fcc86896
Denys <dgolub.ai@gmail.com>
2025-09-02 17:26:54 +0300

    add hand cursor

---
+++ Assets/Scripts/ChangeCursor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, hotSpot, cursorMode);
+    }
+}
